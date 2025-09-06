428a39049330877094bee1e1cebbc0d14a0801b8
realdevwoong <projung134@gmail.com>
2025-09-06 10:17:41 +0900

    concept.py update

--- CRUD/concept/concept1.py
+++ CRUD/concept/concept1.py
@@ -1,2 +1,2 @@
-import test2
+import concept2
 print("test 실행")
--- CRUD/concept/concept2.py
+++ CRUD/concept/concept2.py
@@ -1,5 +1,5 @@
 def hello():
     print("Hello")
-
-if __name__ == "__main__":
-    hello()
+print("Hello")
+# if __name__ == "__main__":
+#     hello()
